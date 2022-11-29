81d1696217d5b382cb5c67f928a59e28833477bf
juan-rv <juanrodriguez9502@gmail.com>
2022-11-28 22:08:16 -0500

    deploy final

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
     console.log(`the server is runing in port ${port}`); // eslint-disable-line no-console
   });
--- api/src/db.js
+++ api/src/db.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
+const {DB_DEPLOY
 } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
